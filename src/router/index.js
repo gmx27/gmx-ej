3a56d7469b3cb8a16c705b455349df46e74d1104
gmx27 <1059122694@qq.com>
2020-01-04 10:04:11 +0800

    index

--- src/router/index.js
+++ src/router/index.js
@@ -101,6 +101,42 @@ export const constantRoutes = [
       }
     ]
   },
+  {
+    path: '/address',
+    component: Layout,
+    children: [
+      {
+        path: 'list',
+        name: 'AddressList',
+        component: () => import('@/pages/address/List'),
+        meta: { title: '地址管理', icon: 'user' }
+      }
+    ]
+  },
+  {
+    path: '/comment',
+    component: Layout,
+    children: [
+      {
+        path: 'list',
+        name: 'CommentList',
+        component: () => import('@/pages/comment/List'),
+        meta: { title: '评论管理', icon: 'user' }
+      }
+    ]
+  },
+  {
+    path: '/orderForm',
+    component: Layout,
+    children: [
+      {
+        path: 'list',
+        name: 'orderFormList',
+        component: () => import('@/pages/orderForm/List'),
+        meta: { title: '订单管理', icon: 'user' }
+      }
+    ]
+  },
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
